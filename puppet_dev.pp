8fc4654a2530a9dad741c6e59a1ccc8ad950e58c
Andrei Cristian Petcu <andrei@ceata.org>
2014-10-11 14:06:48 +0300

    removed duplicate puppet packages

--- puppet_dev.pp
+++ puppet_dev.pp
@@ -1,3 +1,3 @@
 Package { ensure =>  "installed" }
-$my_packages = [ "tmux", "emacs", "vim", "ctags", "ack", "git", "ranger", "ack", "nmap", "git", "mc", "zsh", "wget", "curl", "rsync", "htop", "iotop", "lsof", "llvm", "make", "nmon"]
+$my_packages = [ "tmux", "emacs", "vim", "ctags", "ack", "git", "ranger", "nmap", "mc", "zsh", "wget", "curl", "rsync", "htop", "iotop", "lsof", "llvm", "make", "nmon"]
 package { $my_packages: }
