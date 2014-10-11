442a34e1e0cb1d8eba5a8729bf2532dc80706fa9
Andrei Cristian Petcu <andrei@ceata.org>
2014-10-11 14:41:02 +0300

    add os specific support for puppet packages

--- puppet_dev.pp
+++ puppet_dev.pp
@@ -1,3 +1,13 @@
 Package { ensure =>  "installed" }
 $my_packages = [ "tmux", "emacs", "vim", "ctags", "ack", "git", "ranger", "nmap", "mc", "zsh", "wget", "curl", "rsync", "htop", "iotop", "lsof", "llvm", "make", "nmon"]
 package { $my_packages: }
+
+case $operatingsystem {
+  # centos: { $os_specific_packages = "httpd" }
+  # Note that these matches are case-insensitive.
+  # redhat: { $os_specific_packages = "httpd" }
+  debian: { $os_specific_packages = "apache2" }
+  ubuntu: { $os_specific_packages = "ack-grep" }
+  default: { fail("Unrecognized operating system " + $operatingsystem) }
+}
+package { $os_specific_packages: }
