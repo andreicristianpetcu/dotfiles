44ae2925da4de5517d69572938884c2588df103c
Andrei Cristian Petcu <andrei@ceata.org>
2014-10-11 14:42:41 +0300

    removed ack from global package list

--- puppet_dev.pp
+++ puppet_dev.pp
@@ -1,12 +1,12 @@
 Package { ensure =>  "installed" }
-$my_packages = [ "tmux", "emacs", "vim", "ctags", "ack", "git", "ranger", "nmap", "mc", "zsh", "wget", "curl", "rsync", "htop", "iotop", "lsof", "llvm", "make", "nmon"]
+$my_packages = [ "tmux", "emacs", "vim", "ctags", "git", "ranger", "nmap", "mc", "zsh", "wget", "curl", "rsync", "htop", "iotop", "lsof", "llvm", "make", "nmon"]
 package { $my_packages: }
 
 case $operatingsystem {
   # centos: { $os_specific_packages = "httpd" }
   # Note that these matches are case-insensitive.
   # redhat: { $os_specific_packages = "httpd" }
-  debian: { $os_specific_packages = "apache2" }
+  debian: { $os_specific_packages = "ack-grep" }
   ubuntu: { $os_specific_packages = "ack-grep" }
   default: { fail("Unrecognized operating system " + $operatingsystem) }
 }
