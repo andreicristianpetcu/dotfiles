28c159259d7ce36a0d114ab84166c1c3b7e2b6eb
Andrei Cristian Petcu <andrei@ceata.org>
2014-10-14 23:07:41 +0300

    added all the fancy build tools for developers

--- puppet_dev.pp
+++ puppet_dev.pp
@@ -1,11 +1,12 @@
-Package { ensure =>  "installed" }
+Package { ensure =>  "installed" }                                                                                                                                                                                                      
 $common_packages = [ "tmux", "emacs", "ctags", "git", "ranger", "nmap", "mc", "zsh", "wget", "curl",
     "rsync", "htop", "iotop", "lsof", "llvm", "make", "nmon", "autojump", "elinks", "ruby", "python", "nodejs"]
-
-$debian_packages = ["vim", "ack-grep", "silversearcher-ag", "telnet"]
-$ubuntu_packages = ["vim", "ack-grep", "silversearcher-ag", "telnet"]
-$archgnulinux_packages = ["gvim", "ack", "silver-searcher-git", "inetutils" ]
-
+      
+$ubuntu_packages = ["vim", "ack-grep", "silversearcher-ag", "telnet", "autoconf", "bison", "build-essential", 
+    "libssl-dev", "libyaml-dev", "libreadline6-dev", "zlib1g-dev", "libncurses5-dev"]
+$debian_packages = $ubuntu_packages
+$archgnulinux_packages = ["gvim", "ack", "silver-searcher-git", "inetutils", "base-devel"]
+      
 case $operatingsystem {
   # centos: { $os_specific_packages = "httpd" }
   # Note that these matches are case-insensitive.
@@ -14,12 +15,12 @@ case $operatingsystem {
   ubuntu: { $os_specific_packages = $ubuntu_packages }
   Archlinux: { $os_specific_packages = $archgnulinux_packages }
   default: { fail("Unrecognized operating system $operatingsystem") }
-}
-
+}     
+      
 package { $common_packages: }
 package { $os_specific_packages: }
-
+      
 package { 'bundler':
     ensure   => 'installed',
     provider => 'gem',
-}
+}     
