57e7dfe23789947cc7cbc248640c4085f4ba7b59
Andrei Cristian Petcu <andrei@ceata.org>
2015-09-28 18:52:08 +0300

    atom changes

---
+++ atom/compile-cache/js/babel/8e7236f6d23825690647a9a68c5756da9fc488b8/1ca6bebdfdab84549b8c5b5a43cb03846df63171.js
@@ -0,0 +1,13 @@
+'use babel';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports['default'] = {
+  getProcessPlatform: function getProcessPlatform() {
+    return process.platform;
+  }
+};
+module.exports = exports['default'];
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlcy9saW5lLWVuZGluZy1zZWxlY3Rvci9saWIvaGVscGVycy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxXQUFXLENBQUE7O0FBRVgsTUFBTSxDQUFDLGNBQWMsQ0FBQyxPQUFPLEVBQUUsWUFBWSxFQUFFO0FBQzNDLE9BQUssRUFBRSxJQUFJO0NBQ1osQ0FBQyxDQUFDO0FBQ0gsT0FBTyxDQUFDLFNBQVMsQ0FBQyxHQUhIO0FBQ2Isb0JBQWtCLEVBQUMsU0FBQSxrQkFBQSxHQUFHO0FBQ3BCLFdBQU8sT0FBTyxDQUFDLFFBQVEsQ0FBQTtHQUN4QjtDQUNGLENBQUE7QUFJRCxNQUFNLENBQUMsT0FBTyxHQUFHLE9BQU8sQ0FBQyxTQUFTLENBQUMsQ0FBQyIsImZpbGUiOiJub2RlX21vZHVsZXMvbGluZS1lbmRpbmctc2VsZWN0b3IvbGliL2hlbHBlcnMuanMiLCJzb3VyY2VzQ29udGVudCI6WyIndXNlIGJhYmVsJ1xuXG5leHBvcnQgZGVmYXVsdCB7XG4gIGdldFByb2Nlc3NQbGF0Zm9ybSAoKSB7XG4gICAgcmV0dXJuIHByb2Nlc3MucGxhdGZvcm1cbiAgfVxufVxuIl19
+//# sourceURL=/usr/share/atom/resources/app.asar/node_modules/line-ending-selector/lib/helpers.js
