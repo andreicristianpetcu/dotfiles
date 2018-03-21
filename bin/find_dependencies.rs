279165546697f79aa3223d1e6b8d7ea312abc7c1
Andrei Cristian Petcu <andrei@ceata.org>
2018-03-21 21:00:38 +0200

    added unit tests

--- bin/find_dependencies.rs
+++ bin/find_dependencies.rs
@@ -1,5 +1,21 @@
 #!/usr/bin/env run-cargo-script
+// cargo-deps: time="0.1.25"
+// You can also leave off the version number, in which case, it's assumed
+// to be "*".  Also, the `cargo-deps` comment *must* be a single-line
+// comment, and it *must* be the first thing in the file, after the
+// hashbang.
+extern crate time;
 
 fn main() {
-    println!("Hello, World!");
+    println!("Hello, World!3");
+    println!("{}", time::now().rfc822z());
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
